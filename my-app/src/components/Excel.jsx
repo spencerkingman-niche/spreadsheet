a56c896a8bef18580599a0e2c4ca3aab7acb2e6e
spencerkingman-niche <skingman@niche.com>
2017-08-20 23:16:30 -0400

    Added sort on click of column headers. Added edit on double click of a cell. Added save on ENTER.

--- my-app/src/components/Excel.jsx
+++ my-app/src/components/Excel.jsx
@@ -6,34 +6,95 @@ class Excel extends Component {
     constructor() {
         super()
         this.state = {
-            data : []
+            data: [],
+            edit: {
+                row : null,
+                cell : null
+            },
+            sortBy: null,
+            ascending: true
         }
+        this._save = this._save.bind(this)
+        this._sort = this._sort.bind(this)
+        this._showEditor = this._showEditor.bind(this)
     }
 
     componentWillMount() {
         this.setState({data: this.props.initialData})
     }
 
-    render() {
-            console.log(this.props)
+    _save(e) {
+        e.preventDefault()
+        const input = e.target.firstChild
+        var data = this.state.data.slice()
+        data[this.state.edit.row][this.state.edit.cell] = input.value
+        this.setState({
+            edit: {
+                row : null,
+                cell : null
+            }, 
+            data: data,
+        })
+    }
 
+    _showEditor(e) {
+        e.preventDefault()
+        const row = parseInt(e.target.dataset.row, 10)
+        const column = e.target.cellIndex
+        this.setState({
+            edit : {
+                row : row,
+                cell : column
+            }
+        })
+    }
+
+    _sort(e) {
+        e.preventDefault()
+        const column = e.target.cellIndex
+        let data = this.state.data.slice()
+        let ascending = (this.state.sortBy === column) && !(this.state.ascending)
+        data.sort((a, b) => (ascending ? 
+                                (a[column]<b[column] ? 1 : -1) :
+                                (a[column]>b[column] ? 1 : -1)
+                                ))
+        this.setState({
+            data: data,
+            sortBy: column,
+            ascending: ascending
+        })
+    }
+
+    render() {
+        const edit = this.state.edit
         return (
             <table className="table">
                 <thead>
-                    <tr>
-                        {this.props.headers.map( (title, i) => (<th key={i}>{title}</th>) )}
+                    <tr onClick={this._sort}>
+                        {this.props.headers.map((title, i) => {
+                            if (this.state.sortBy === i) {
+                                title += (this.state.ascending ? ' \u2191' : ' \u2193')
+                            }
+                            return (<th key={i}>{title}</th>) 
+                        })} 
                     </tr>
                 </thead>
-                <tbody>
+                <tbody onDoubleClick={this._showEditor}>
                     {this.state.data.map( (row, i) => (
                         <tr key={i}>
-                            {row.map( (cell, i) => (
-                                <td key={i}>
-                                    {cell}
-                                </td>))}
+                            {row.map( (cell, j) => {
+                                if (edit && edit.row === i && edit.cell === j) {
+                                    return(
+                                        <td key={j} data-row={i}>
+                                            <form onSubmit={this._save}>
+                                                <input type='text' defaultValue={this.state.data[i][j]} />
+                                            </form>
+                                        </td>)
+                                }
+                                return (<td key={j} data-row={i}>{cell}</td>)
+                            })}     
                         </tr>
-                    ))}
-                    
+                    ))}  
                 </tbody>
             </table>
         )
